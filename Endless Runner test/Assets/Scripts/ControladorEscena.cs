e77601b6e1ce70f127a29b2ee0551630e16c5ba2
Jonathan Córdova <jcordova3@santotomas.cl>
2020-05-22 23:50:26 -0400

    Version testing final

---
+++ Endless Runner test/Assets/Scripts/ControladorEscena.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ControladorEscena : MonoBehaviour
+{
+    public GameObject canvasGameOver;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Time.timeScale = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void perdiste()
+    {
+        canvasGameOver.SetActive(true);
+        Time.timeScale = 0;
+    }
+
+    public void reiniciar()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
---
+++ Endless Runner test/Assets/Scripts/GeneradorTuberias.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GeneradorTuberias : MonoBehaviour
+{
+    public GameObject tuberia;
+    public float tiempoMax = 1;
+    private float tiempoActual = 0;
+    public float altura = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject tuberiaNueva = Instantiate(tuberia);
+        tuberiaNueva.transform.position += new Vector3(0, 0, 0);
+        Destroy(tuberiaNueva, 10);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(tiempoActual > tiempoMax)
+        {
+            GameObject tuberiaNueva = Instantiate(tuberia);
+            tuberiaNueva.transform.position += new Vector3(0, Random.Range(-altura, altura), 0);
+            Destroy(tuberiaNueva, 10);
+            tiempoActual = 0;
+            tiempoMax = Random.Range(1,3);
+} else
+        {
+            tiempoActual += Time.deltaTime;
+        }
+        
+    }
+}
